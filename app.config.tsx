7bccbb7aabd9bc024c1d93d5df1d0158a2f395ee
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2020-09-09 14:39:05 +0530

    Initial Commit

---
+++ app.config.tsx
@@ -0,0 +1,5 @@
+export default {
+    appName: "PushDemo",
+    env: "production",
+    apiUrl: "https://colorfyn-dev-as.azurewebsites.net/api/"
+  };
---
+++ src/services/DemoNotificationRegistrationService.tsx
@@ -0,0 +1,53 @@
+import { Alert } from "react-native";
+
+export default class DemoNotificationService {
+    constructor(
+        readonly apiUrl: string,
+        readonly apiKey: string) {
+
+    }
+
+    async registerAsync(request: any): Promise<Response> {
+        console.log("Helelo");
+        console.log(request);
+        console.log("Helelo2");
+        console.log(JSON.stringify(request));
+        const method = 'PUT';
+        const registerApiUrl = `${this.apiUrl}notifications/installations`;
+        console.log(registerApiUrl);
+        const result = await fetch(registerApiUrl, {
+            method: method,
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(request)
+        });
+        this.validateResponse(registerApiUrl, method, request, result);
+        console.log("reurning result");  
+        return result;
+    }
+
+    async deregisterAsync(deviceId: string): Promise<Response> {
+        const method = 'DELETE';
+        const deregisterApiUrl = `${this.apiUrl}/notifications/installations/${deviceId}`;
+        const result = await fetch(deregisterApiUrl, {
+            method: method,
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+                'apiKey': this.apiKey
+            }
+        });
+
+        this.validateResponse(deregisterApiUrl, method, null, result);
+        return result;
+    }
+
+    private validateResponse(requestUrl: string, method: string, requestPayload: any, response: Response) {
+        console.log(`Request: ${method} ${requestUrl} => ${JSON.stringify(requestPayload)}\nResponse: ${response.status}`);
+        if (!response || response.status != 200) {
+            throw `HTTP error ${response.status}: ${response.statusText}`;
+        }
+    }
+}
